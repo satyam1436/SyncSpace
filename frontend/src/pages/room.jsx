7b3bd10089845b18d6abb3bd02da81270c3268c0
Vivek Kumar <vivek15092000@gmail.com>
2026-07-22 03:45:53 +0530

    feat: add basic Room page structure

---
+++ frontend/src/pages/room.jsx
@@ -0,0 +1,10 @@
+function Room() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Welcome to SyncSpace Room</h1>
+      <p>This is where the whiteboard and code editor will go.</p>
+    </div>
+  );
+}
+
+export default Room;
