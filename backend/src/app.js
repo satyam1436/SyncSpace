6177699320bb936fe0e7fdb69e74d32c2f7ccdea
Satyam Kumar <111216715+satyam1436@users.noreply.github.com>
2026-08-08 00:17:48 +0530

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,6 +16,7 @@ app.use(
     })
 );
 
+
 // Parse incoming JSON request bodies
 app.use(express.json({ limit: "10kb" }));
 
@@ -49,6 +50,7 @@ app.use((req, res, next) => {
 
     next(error);
 });
+
 
 // Global Error Handler
 app.use(errorHandler);
